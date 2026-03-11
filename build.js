173e1c2faaeb2d37b0839383e7e6981640577dd5
hrkcz001 <hrkcz001@gmail.com>
2026-03-11 05:00:39 +0100

    Фикс для нового домена shikimori.io

--- build.js
+++ build.js
@@ -3,7 +3,7 @@
 const CONFIG = {
   resources: {
     name: "Shikiplayer",
-    version: "5.4.1",
+    version: "5.4.2",
     author: "Kaneko Qt",
     homepage_url: "https://github.com/qt-kaneko/Shikiplayer",
     description: "Adds Kodik player to Shikimori website",
